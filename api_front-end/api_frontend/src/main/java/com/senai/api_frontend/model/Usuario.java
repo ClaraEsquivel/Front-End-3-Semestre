c7788b9087ac22f693d20214ed3dece8b91c84a8
Clara Esquivel <160122503+ClaraEsquivel@users.noreply.github.com>
2025-04-29 11:35:00 -0300

    Add files via upload

---
+++ api_front-end/api_frontend/src/main/java/com/senai/api_frontend/model/Usuario.java
@@ -0,0 +1,80 @@
+package com.senai.api_frontend.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+@Entity
+public class Usuario {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    @NotBlank(message = "Nome é obrigatório")
+    private String nome;
+
+    @Column(nullable = false)
+    @NotBlank(message = "E-mail é obrigatório")
+    @Email(message = "Deve ser um e-mail válido")
+    private String email;
+
+    @Column(nullable = false)
+    @NotBlank(message = "Senha é obrigatória")
+    @Size(min = 3, message = "A senha deve ter no mínimo 3 caracters.")
+    private String senha;
+
+    public Usuario() {
+    }
+
+    public Usuario(Long id, @NotBlank(message = "Nome é obrigatório") String nome,
+                   @NotBlank(message = "E-mail é obrigatório") @Email(message = "Deve ser um e-mail válido") String email,
+                   @NotBlank(message = "Senha é obrigatória") @Size(min = 3, message = "A senha deve ter no mínimo 3 caracters.") String senha) {
+        this.id = id;
+        this.nome = nome;
+        this.email = email;
+        this.senha = senha;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    @Override
+    public String toString() {
+        return "Usuario [id=" + id +
+                ", nome=" + nome +
+                ", email=" + email +
+                ", senha=" + senha + "]";
+    }
+}
+
---
+++ api_front-end/api_frontend/src/test/java/com/senai/api_frontend/ApiFrontendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.senai.api_frontend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiFrontendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
