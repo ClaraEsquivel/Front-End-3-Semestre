bd05057d9db7c5581a723be7689ddb7a7510cc0e
Clara Esquivel <160122503+ClaraEsquivel@users.noreply.github.com>
2025-05-05 08:20:29 -0300

    Update UsuarioController.java

--- api_front-end/api_frontend/src/main/java/com/senai/api_frontend/controller/UsuarioController.java
+++ api_front-end/api_frontend/src/main/java/com/senai/api_frontend/controller/UsuarioController.java
@@ -1,7 +1,7 @@
 package com.senai.api_frontend.controller;
 
-import com.senai.api_frontend.model.Usuario;
-import com.senai.api_frontend.service.UsuarioService;
+import com.senai.api_atividade_senai.model.Usuario;
+import com.senai.api_atividade_senai.service.UsuarioService;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -10,27 +10,28 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import java.util.Map;
 
-@CrossOrigin(origins = "*") //Aceita conexão de aplicações externas
+@CrossOrigin(origins = "*") // Aceita conexão de aplicações externas.
 @RestController
 @RequestMapping("/usuarios")
-
 public class UsuarioController {
     private UsuarioService usuarioService;
 
-    public UsuarioController(UsuarioService usuarioService){
+    public UsuarioController(UsuarioService usuarioService) {
         this.usuarioService = usuarioService;
     }
 
     @GetMapping
-    public List<Usuario> listarTodos(){
+    public List<Usuario> listarTodos() {
         return usuarioService.listarTodos();
     }
 
     @PostMapping
-    public ResponseEntity<Map<String, Object>> salvar(@Valid @RequestBody Usuario usuario){
+    public ResponseEntity<Map<String, Object>> salvar(@Valid @RequestBody Usuario usuario) {
         usuarioService.salvar(usuario);
         return ResponseEntity
                 .status(HttpStatus.CREATED)
-                .body(Map.of("mensagem", "Usuário cadastrado com sucesso"));
+                .body(Map.of("mensagem", "Usuario cadastrado com sucesso."));
     }
+
 }
+
