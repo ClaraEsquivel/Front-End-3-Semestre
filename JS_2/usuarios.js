3d4eba5d7145d7ec0b6bf467881cc565025c9fdd
Clara Esquivel <160122503+ClaraEsquivel@users.noreply.github.com>
2025-05-05 08:31:34 -0300

    Update usuarios.js

--- JS_2/usuarios.js
+++ JS_2/usuarios.js
@@ -1,31 +1,31 @@
 document.addEventListener('DOMContentLoaded', carregarUsuarios)
 
-const carregarUsuarios = () => {
-    fetch('http://localhost:8080/usuarios ')
-    .then( response => {
-        if (!response.ok){
-            throw new Error('Erro ao buscar usuários. ')
+function carregarUsuarios() {
+    fetch('http://localhost:8080/usuarios')
+    .then(response => {
+        if (!response.ok) {
+            throw new Error("Erro ao buscar usuários");
         }
-        response.json()
+        return response.json()
     })
     .then(usuarios => {
         const lista = document.getElementById('listaUsuarios')
-        
-        if (usuarios.length === 0) {
+        lista.innerHTML = ''
+
+        if (usuarios.lenght === 0) {
             lista.innerHTML = '<li>Nenhum usuário encontrado. </li>'
-            return
+            return;
         }
-        
+
         usuarios.forEach(usuario => {
             const item = document.createElement('li')
-            item.innerHTML = `<strong>Nome: </strong> ${usuario.nome}<br>
-            <strong>E-mail: </strong> ${usuario.email}`
+            item.innerHTML = `<strong>Nome:</strong> ${usuario.nome}<br>
+                            <strong>E-mail:</strong> ${usuario.email}`
             lista.appendChild(item)
         })
-
     })
-    .catch(erro => {
-        document.getElementById('listaUsuarios').innerHTML =
-        '<li>Erro ao cadastrar usuários.</li>'
+    .catch( erro => {
+        document.getElementById('listaUsuarios').innerHTML = 
+        '<li>Erro ao carregar usuários.</li>'
     })
 }
