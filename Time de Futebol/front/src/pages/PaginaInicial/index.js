8acffef890b432a8092a766ce16208532f53a7db
Clara Esquivel <160122503+ClaraEsquivel@users.noreply.github.com>
2025-05-20 11:27:25 -0300

    Add files via upload

---
+++ Time de Futebol/front/src/pages/PaginaInicial/index.js
@@ -0,0 +1,15 @@
+// src\pages\PaginaInicial\index.js
+// import React from 'react';
+// import FormularioCadastro from '../FormularioCadastro'; // Importa o componente de FormularioCadastro
+
+// const PaginaInicial = () => {
+//     return (
+//         <div>
+//             <h1>Bem-vindo à Página Inicial</h1>
+//             <p>Preencha o formulário abaixo para começar:</p>
+//             <FormularioCadastro /> {/* Renderiza o componente FormularioCadastro */}
+//         </div>
+//     );
+// };
+
+// export default PaginaInicial;
